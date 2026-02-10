91c1e9d58c28bbe6afd378e97c4381ab78819367
CILSON01 <ncilson11@gmail.com>
2026-02-10 15:28:20 +0530

    Add files via upload

---
+++ even and odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+
+    printf("Enter the number: ");
+    scanf("%d", &num1);
+
+    if (num1 /2) {
+        printf("the number entered is even.\n");
+    } else {
+        printf("The numer entered is odd .\n");
+    }
+
+    return 0;
+}
