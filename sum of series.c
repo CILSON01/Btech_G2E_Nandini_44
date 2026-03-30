9818f895b5b5bb8db553eb4ea12fd472f12f1131
CILSON01 <ncilson11@gmail.com>
2026-03-31 01:22:03 +0530

    Add files via upload

---
+++ sum of series.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main(){
+    int n,i;
+    float sum = 0.0;
+    printf("Enter the number of terms:");
+    scanf("%d", &n);
+
+for(i=1; i<=n; i++){
+    sum=sum+(1.0/i);
+}
+printf("Sum of the series=%f",sum);
+return 0;
+}
